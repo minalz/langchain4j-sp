ee0c761a8ff436c85d392695ab9624876412451a
minalz-pc <119687281@qq.com>
2025-06-23 23:43:07 +0800

    feat: add 流式调用 非阻塞

--- src/main/java/cn/minalz/aiservice/ConsultantService.java
+++ src/main/java/cn/minalz/aiservice/ConsultantService.java
@@ -2,13 +2,17 @@ package cn.minalz.aiservice;
 
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
+import reactor.core.publisher.Flux;
 
 @AiService(
         wiringMode = AiServiceWiringMode.EXPLICIT, // 手动装配
-        chatModel = "openAiChatModel" // 指定模型
+        chatModel = "openAiChatModel", // 指定模型
+        streamingChatModel = "openAiStreamingChatModel" // 流式调用 非阻塞
 )
 //@AiService // 默认就可以使用，默认用的是自动装配，用上面的可以使得代码更为明晰，当然了，不加配置的代码更为简洁
 public interface ConsultantService {
 
-    String chat(String message);
+//    String chat(String message);
+
+    Flux<String> chat(String message);
 }
--- src/main/java/cn/minalz/controller/ChatController.java
+++ src/main/java/cn/minalz/controller/ChatController.java
@@ -4,6 +4,7 @@ import cn.minalz.aiservice.ConsultantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ChatController {
@@ -11,10 +12,15 @@ public class ChatController {
     @Autowired
     private ConsultantService consultantService;
 
-    @GetMapping("/chat")
-    public String chat(String message) {
+    @GetMapping(value = "/chat", produces = "text/html;charset=utf-8")
+    public Flux<String> chat(String message) {
         return consultantService.chat(message);
     }
+
+    /*@GetMapping("/chat")
+    public String chat(String message) {
+        return consultantService.chat(message);
+    }*/
 
     /*@Autowired
     private OpenAiChatModel chatModel;
