2680863f458d56f1bd1e453d675d3b9d6c8c6994
minalz-pc <119687281@qq.com>
2025-06-24 23:09:50 +0800

    feat: add 会话记忆

--- src/main/java/cn/minalz/aiservice/ConsultantService.java
+++ src/main/java/cn/minalz/aiservice/ConsultantService.java
@@ -10,7 +10,8 @@ import reactor.core.publisher.Flux;
 @AiService(
         wiringMode = AiServiceWiringMode.EXPLICIT, // 手动装配
         chatModel = "openAiChatModel", // 指定模型
-        streamingChatModel = "openAiStreamingChatModel" // 流式调用 非阻塞
+        streamingChatModel = "openAiStreamingChatModel", // 流式调用 非阻塞
+        chatMemory = "chatMemory"
 )
 //@AiService // 默认就可以使用，默认用的是自动装配，用上面的可以使得代码更为明晰，当然了，不加配置的代码更为简洁
 public interface ConsultantService {
@@ -20,6 +21,9 @@ public interface ConsultantService {
 //    @SystemMessage("你是天下第一的智能小助手朱朱,人美心善又多金")
 //    @SystemMessage(fromResource = "system.txt")
 //    @UserMessage("你是天下第一的智能小助手朱朱,人美心善又多金!{{it}}")
-    @UserMessage("你是天下第一的智能小助手朱朱,人美心善又多金!{{msg}}")
-    Flux<String> chat(@V("msg") String message);
+//    @UserMessage("你是天下第一的智能小助手朱朱,人美心善又多金!{{msg}}")
+//    Flux<String> chat(@V("msg") String message);
+
+    @SystemMessage(fromResource = "system.txt")
+    Flux<String> chat(String message);
 }
--- src/main/java/cn/minalz/config/CommonConfig.java
+++ src/main/java/cn/minalz/config/CommonConfig.java
@@ -1,6 +1,8 @@
 package cn.minalz.config;
 
 import cn.minalz.aiservice.ConsultantService;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.service.AiServices;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,4 +21,12 @@ public class CommonConfig {
                 .chatModel(chatModel)
                 .build();
     }*/
+
+    // 构建会话记忆对象
+    @Bean
+    public ChatMemory chatMemory() {
+        return MessageWindowChatMemory.builder()
+                .maxMessages(20)
+                .build();
+    }
 }
