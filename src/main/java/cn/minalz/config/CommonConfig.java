0870ad640e77589432a07a5e6e8ef7c2cdb0fd1d
minalz-pc <119687281@qq.com>
2025-06-25 23:49:44 +0800

    feat: add RAG and 向量数据库

--- src/main/java/cn/minalz/config/CommonConfig.java
+++ src/main/java/cn/minalz/config/CommonConfig.java
@@ -1,15 +1,26 @@
 package cn.minalz.config;
 
-import cn.minalz.aiservice.ConsultantService;
+import dev.langchain4j.community.store.embedding.redis.RedisEmbeddingStore;
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.document.loader.ClassPathDocumentLoader;
+import dev.langchain4j.data.document.parser.apache.pdfbox.ApachePdfBoxDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
-import dev.langchain4j.service.AiServices;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import dev.langchain4j.store.memory.chat.ChatMemoryStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class CommonConfig {
@@ -18,6 +29,10 @@ public class CommonConfig {
     private OpenAiChatModel chatModel;
     @Autowired
     private ChatMemoryStore redisChatMemoryStore;
+    @Autowired
+    private EmbeddingModel embeddingModel;
+    @Autowired
+    private RedisEmbeddingStore redisEmbeddingStore;
 
     /*@Bean
     public ConsultantService consultantService() {
@@ -48,5 +63,42 @@ public class CommonConfig {
                         .build();
             }
         };
+    }
+
+    // 构建向量数据库操作对象
+    @Bean
+    public EmbeddingStore myEmbeddingStore() {
+        // 1.加载文档进内存
+//        List<Document> documents = ClassPathDocumentLoader.loadDocuments("content");
+        List<Document> documents = ClassPathDocumentLoader.loadDocuments("content", new ApachePdfBoxDocumentParser());
+        // 绝对路径 absolute path
+//        List<Document> documents = FileSystemDocumentLoader.loadDocuments("D:\\BBBWorkSpace\\langchain4j-sp\\src\\main\\resources\\content");
+        // 2.构建向量数据库操作对象
+        // 构建文档分割器对象
+        DocumentSplitter ds = DocumentSplitters.recursive(500, 100);
+        // 内存向量数据库
+//        InMemoryEmbeddingStore store = new InMemoryEmbeddingStore();
+        // 3.构建一个EmbeddingStoreIngestor对象，完成文本数据切割，向量化，存储
+        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
+//                .embeddingStore(store)
+                .embeddingStore(redisEmbeddingStore)
+                .documentSplitter(ds)
+                .embeddingModel(embeddingModel)
+                .build();
+        ingestor.ingest(documents);
+//        return store;
+        return redisEmbeddingStore;
+    }
+
+    // 构建向量数据库检索对象
+    @Bean
+    public ContentRetriever contentRetriever(/*EmbeddingStore embeddingStore*/) {
+        return EmbeddingStoreContentRetriever.builder()
+//                .embeddingStore(embeddingStore)
+                .embeddingStore(redisEmbeddingStore)
+                .minScore(0.5)
+                .maxResults(3)
+                .embeddingModel(embeddingModel)
+                .build();
     }
 }
